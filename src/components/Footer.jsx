3ec296f006ea10d9633fb22c1a7b17e6c28256e9
suhani-saxena-23 <saxenasuhani606@gmail.com>
2025-09-25 21:38:10 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => (
     <Typography variant="h5"
      sx={{ fontSize: { lg: '28px', xs: '20px' } }}
       mt="41px" textAlign="center" pb="40px">
-        ❤️Made by Saloni❤️</Typography>
+        ❤️Made by Suhani❤️</Typography>
   </Box>
 );
 
